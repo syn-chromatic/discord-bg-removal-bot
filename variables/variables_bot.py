2e105a5b36e8143b52a4e1c562d60b985348b034
Syn-dromatic <68112904+Syn-dromatic@users.noreply.github.com>
2022-04-25 14:45:54 +0300

    decreased max_px_image value

--- variables/variables_bot.py
+++ variables/variables_bot.py
@@ -4,6 +4,6 @@ ACTIVITY_TEXT = ""
 
 # Rembg Variables
 max_frames_animated = 50 # Limits the amount of frames to process in an animated image or video
-max_px_image = 8000 # Limits the amount of pixels in width or height to process in still images
+max_px_image = 6000 # Limits the amount of pixels in width or height to process in still images
 max_px_animated = 640 # Limits the amount of pixels in width or height to process in animated images or video
 max_video_fps = 15 #
